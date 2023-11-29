e895b35b554ae09e757fa21c5e59f4be98954fb3
Palash Das <ng.palash.das@gmail.com>
2023-11-30 01:12:10 +0530

    completed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -14,38 +14,28 @@ const About = () => {
           className={styles.img}
         />
         <div className={styles.imgText}>
-          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h1 className={styles.imgTitle}>Story Of Me</h1>
           <h2 className={styles.imgDesc}>
-            Handcrafting award winning digital experiences
+            An Aspiring FullStack Developer
           </h2>
         </div>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.item}>
-          <h1 className={styles.title}>Who Are We?</h1>
+          <h1 className={styles.title}>Who am I?</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
-            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
-            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
-            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
-            <br />
-            <br />
-            voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
-            esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
-            officiis voluptatum quo ea eveniet?
+            I am a passionate and self-taught Full-Stack Developer residing in Krishnagar, West Bengal, India. At the age of 20, I have dedicated myself to the exciting world of Web Development and have embarked on a journey of constant learning and growth.
+            <br /><br />
+            From a young age, I was fascinated by technology and its ability to shape the world around us. This curiosity led me to explore programming and Web Development, where I quickly discovered my knack for creating dynamic and user-friendly applications.
+            <br /><br />
+            As a Full-Stack Developer, I possess a diverse skill set that allows me to handle both the Frontend and Backend with Database aspects of Web Development.
           </p>
         </div>
         <div className={styles.item}>
-          <h1 className={styles.title}>What We Do?</h1>
+          <h1 className={styles.title}>What do I Do?</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
-            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
-            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. -
-            Creative Illustrations
+            I made projects to enlarge my knowlegde in the technology and to learn new things.<br/>
+            Currently an intern and the web dev lead.
             <br />
             <br /> - Dynamic Websites
             <br />
@@ -53,7 +43,7 @@ const About = () => {
             <br />
             <br /> - Mobile Apps
           </p>
-          <Button url="/contact" text="Contact" />
+          <Button url="https://iamrealpalash.netlify.app/" text="Contact" />
         </div>
       </div>
     </div>
